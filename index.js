597e89b455cf0018af67a2e9c0aff5f71418867f
kasiacichowska <cichowska8437@gmail.com>
2024-10-26 13:04:15 +0200

    commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+function fibonacci(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    }
+
+    let x = 0;
+    let y = 1;
+    let suma;
+
+    for (let i = 1; i < n; i++) {
+        suma = x + y;
+        x = y;
+        y = suma;
+    }
+
+    return suma;
+}
+
+console.log(fibonacci(20));
