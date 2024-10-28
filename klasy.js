86752d28197225aad005265e5b79cafcbbc9fe24
kasiacichowska <cichowska8437@gmail.com>
2024-10-28 08:45:52 +0100

    commit

---
+++ klasy.js
@@ -0,0 +1,78 @@
+class Produkt {
+    constructor(cena, waga, sklad) {
+        this._cena = cena;
+        this._waga = waga;
+        this._sklad = sklad;
+    }
+
+    get cena() {
+        return this._cena;
+    }
+
+    get waga() {
+        return this._waga;
+    }
+
+    get sklad() {
+        return this._sklad;
+    }
+
+    set cena(cena) {
+        this._cena = cena;
+    }
+
+    set waga(waga) {
+        this._waga = waga;
+    }
+
+    set sklad(sklad) {
+        this._sklad = sklad;
+    }
+
+    toString() {
+        return `CENA: ${this._cena}, WAGA: ${this._waga}, SKLAD: ${this._sklad}`;
+    }
+}
+
+class Kosmetyk extends Produkt {
+    constructor(cena, waga, sklad, typ, zapach, kolor) {
+        super(cena, waga, sklad);
+        this._typ = typ;
+        this._zapach = zapach;
+        this._kolor = kolor;
+    }
+
+    get typ() {
+        return this._typ;
+    }
+
+    get zapach() {
+        return this._zapach;
+    }
+
+    get kolor() {
+        return this._kolor;
+    }
+
+    set typ(typ) {
+        this._typ = typ;
+    }
+
+    set zapach(zapach) {
+        this._zapach = zapach;
+    }
+
+    set kolor(kolor) {
+        this._kolor = kolor;
+    }
+
+    toString() {
+        return `${super.toString()} TYP: ${this._typ}, ZAPACH: ${this._zapach}, KOLOR: ${this.kolor}`;
+    }
+
+}
+
+const produkcik = new Produkt(100, 50, ["woda", "mąka"]);
+console.log(produkcik.toString());
+const szminka = new Kosmetyk(500, 20, ["czerowny barwnik", "wazelina"], "makijaz", "rozany", "czerwony");
+console.log(szminka.toString());
